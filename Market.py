247312501cd8d7594048d174a31f49cf6f260b0a
herostem <ethanmah.stem@gmail.com>
2026-07-27 15:34:30 -0400

    feat: add cancel rates automatic order cancelling

--- Market.py
+++ Market.py
@@ -5,10 +5,12 @@ class Market:
     def __init__(self, 
                  initialBid, initialAsk,
                  LOB = LOB(lotSize=1, epsilon=1, tickSize=1),  
-                 LMT_arrivalParams = {"omega": 1.4, "k": 1.0, 
-                                      "MAX_DISTANCE": 10, "MIN_DISTANCE": 1, "MAX SIZE": 30},
-                 MKT_arrivalParams = {"mu": 0.2, "MAX SIZE": 20},
-                 rerunParams = {"priceDelta": 2.0, "sizeMult": 2}
+                 LMT_arrivalParams = {"omega": 1.3, "k": 1.0, 
+                                      "MAX_DISTANCE": 20, "MIN_DISTANCE": 1, "MAX SIZE": 50},
+                 MKT_arrivalParams = {"mu": 0.4, "MAX SIZE": 30},
+                 rerunParams = {"priceDelta": 2.0, "sizeMult": 2},
+                 Cancel_arrivalParams = {"omega": 0.7 , "k": 1.0, 
+                                         "MAX_DISTANCE": 20, "MIN_DISTANCE": 1}
                  ):
         
         self.LOB = LOB
@@ -21,7 +23,7 @@ class Market:
 
         self.MKT_arrivalParams = MKT_arrivalParams
         self.LMT_arrivalParams = LMT_arrivalParams
-
+        self.Cancel_arrivalParams = Cancel_arrivalParams
         self.rerunParams = rerunParams
 
     def __LMTrateFunction(self, distance, side):
@@ -31,7 +33,7 @@ class Market:
             return 0
         
         elif side == "bid":
-            i = self.LOB.getRelativePrice(distance, "bid")
+            i = self.LOB.getRelativePrice(distance, "bid") # change i to price
             if i is None:
                 return 0
             rate = self.LMT_arrivalParams["k"] / (i ** self.LMT_arrivalParams["omega"])
@@ -40,6 +42,26 @@ class Market:
             if i is None:
                 return 0
             rate = self.LMT_arrivalParams["k"] / (i ** self.LMT_arrivalParams["omega"])
+        else:
+            raise ValueError("Side must be either 'bid' or 'ask'.")
+        return rate
+
+    def __CancelRateFunction(self, distance, side):
+        if distance < self.Cancel_arrivalParams["MIN_DISTANCE"]:
+            return 0
+        if distance > self.Cancel_arrivalParams["MAX_DISTANCE"]:
+            return 0
+        
+        elif side == "bid":
+            price = self.LOB.getRelativePrice(distance, "bid")
+            if price is None:
+                return 0
+            rate = self.Cancel_arrivalParams["k"] / (price ** self.Cancel_arrivalParams["omega"])
+        elif side == "ask":
+            price = self.LOB.getRelativePrice(distance, "ask")
+            if price is None:
+                return 0
+            rate = self.Cancel_arrivalParams["k"] / (price ** self.Cancel_arrivalParams["omega"])
         else:
             raise ValueError("Side must be either 'bid' or 'ask'.")
         return rate
@@ -66,6 +88,24 @@ class Market:
             else:
                 self.LOB.MKTorder(random.randrange(self.LOB.lotSize, self.MKT_arrivalParams["MAX SIZE"] + 1, self.LOB.epsilon))
 
+    def checkCancelRates(self, side):
+        for i in range(self.Cancel_arrivalParams["MIN_DISTANCE"], self.Cancel_arrivalParams["MAX_DISTANCE"] + 1, self.LOB.epsilon): # step by epsilon
+            if side == "bid": # scale by num of orders
+                x = sum(1 for order in self.LOB.orderSet if order.side == "bid" and 
+                        order.price == self.LOB.getRelativePrice(i, "bid"))
+            elif side == "ask":
+                x = sum(1 for order in self.LOB.orderSet if order.side == "ask" and 
+                        order.price == self.LOB.getRelativePrice(i, "ask"))
+            rate = self.__CancelRateFunction(i, side) * x
+            if rate == 0:
+                continue
+
+            if random.random() < rate:
+                for order in self.LOB.orderSet:
+                    if order.side == side and order.price == self.LOB.getRelativePrice(i,   side):
+                        self.LOB.CancelOrder(order)
+                        break
+
     def rerunCheck(self):
         rerun = False
         for item in self.LOB.allStats:
@@ -90,6 +130,8 @@ class Market:
         self.checkLMTrates("bid")
         self.checkLMTrates("ask")
         self.checkMKTrates()
+        self.checkCancelRates("bid")
+        self.checkCancelRates("ask")
         self.tradingPrice = self.LOB.lastPrice
         print(f"Bid: {self.LOB.bidPrice}, Ask: {self.LOB.askPrice}, Last Price: {self.LOB.lastPrice}, Spread: {self.LOB.spread}, Mid Price: {self.LOB.midPrice}") # just for testing
             
