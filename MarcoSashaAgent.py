d8a89dd2d4799480dcc0c3fe8b9025a6307a6ba4
herostem <ethanmah.stem@gmail.com>
2026-07-29 22:56:57 -0400

    feat: add dynamic sizing of orders

--- MarcoSashaAgent.py
+++ MarcoSashaAgent.py
@@ -1,16 +1,21 @@
 from Market import Market
 import math
 
-class MarcoSashaAgent:
-    def __init__(self, LOB, riskAversion = 0.1, timeHorizon = 1):
+class MarcoSashaAgent: # is not concerned with P&L
+    def __init__(self, LOB, omega, riskAversion = 0.1, timeHorizon = 1, stdSize = 20, maxINV = 200):
         self.LOB = LOB
 
         self.riskAversion = riskAversion
         self.timeHorizon = timeHorizon
-        self.omega = self.market.LMT_arrivalParams["omega"]
+        self.omega = omega # use the market LMT arrival param
         self.volatility = 1 # will implement in market later
 
         self.inventory = [] # all current orders
+        self.currentBidPrice = 0
+        self.currentAskPrice = 0
+
+        self.stdSize = stdSize
+        self.maxINV = maxINV
 
     @property
     def shares(self): # positive for long, negative for short
@@ -21,3 +26,15 @@ class MarcoSashaAgent:
 
     def optimalSpread(self):
         return self.riskAversion * self.volatility ** 2 * self.timeHorizon + (2 / self.omega) * math.log(1 + (self.riskAversion / self.omega))
+    def calcSizes(self):
+        pctINV = self.shares / self.maxINV
+        # if shares positive, bid size decreases, ask size increases
+        # if shares negative, bid size increases, ask size decreases
+        bidSize = ((self.stdSize * (1 - pctINV)) / self.LOB.epsilon) * self.LOB.epsilon
+        askSize = ((self.stdSize * (1 + pctINV)) / self.LOB.epsilon) * self.LOB.epsilon
+
+        if abs(self.shares) >= self.maxINV:
+            bidSize = 0
+            askSize = 0
+
+        return max(bidSize, self.LOB.lotSize), -1 * max(askSize, self.LOB.lotSize)
